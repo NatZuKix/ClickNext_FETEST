37d0ccedfc80399201735b47de2f7fde0863c7bd
NatZuKix <nutthanon.nutzuki@mail.kmutt.ac.th>
2023-09-01 23:18:38 +0700

    mobile

--- src/components/MOBILE/footinfomation.vue
+++ src/components/MOBILE/footinfomation.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-    <div class="h-56  w-full flex flex-col justify-start contex bg-gradient-to-b from-[#0E044B] to-[#2C309B]  items-center">
+    <div class="h-60  w-full flex flex-col justify-start contex bg-gradient-to-b from-[#0E044B] to-[#2C309B]  items-center">
       <h1 class="text-white head mt-9 text-center">สำรองที่นั่งล่วงหน้า<span class="text-[#BAD0F2] head2">&nbsp;(จำนวนจำกัด*)</span>
         <br><span class="text-[#BAD0F2] head3">หมายเหตุ : สัมมนา Exclusive สำหรับกลุ่มโรงพยาบาล และสาธารณะสุข</span></h1>
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdf-Px9QA76gCH1byd_sV1aTEsOJkd_bGDPHt8_tqhDKBhGMw/viewform" 
-        target="_blank" class=" w-1/4 h-12 bg-gradient-to-b from-[#0BB64C] to-[#27D96B] rounded-xl mt-3 text-2xl font-noto text-white text-center flex justify-center items-center">
+        target="_blank" class=" w-1/4 h-20 bg-gradient-to-b from-[#0BB64C] to-[#27D96B] rounded-xl mt-3 text-2xl font-noto text-white text-center flex justify-center items-center">
         ลงทะเบียน
         </a>
       </div>
